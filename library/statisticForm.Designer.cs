2221b040abd12245eb27a5ad29532933f2052000
Evgeniy Mukhin <muhin@intervolga.ru>
2022-05-14 14:51:50 +0300

    edit statistic form

--- library/statisticForm.Designer.cs
+++ library/statisticForm.Designer.cs
@@ -31,8 +31,16 @@ namespace library
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(statisticForm));
             this.Label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.Print = new System.Windows.Forms.Button();
+            this.totalBooksLabel = new System.Windows.Forms.Label();
+            this.unavailableBooksCountLabel = new System.Windows.Forms.Label();
+            this.availableBooksCountLabel = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.totalUsersCountLabel = new System.Windows.Forms.Label();
+            this.readersCountLabel = new System.Windows.Forms.Label();
+            this.employeesCountLabel = new System.Windows.Forms.Label();
+            this.mostActiveReaderLabel = new System.Windows.Forms.Label();
+            this.mostPopularBookLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // Label1
@@ -41,22 +49,13 @@ namespace library
             this.Label1.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Label1.Location = new System.Drawing.Point(12, 9);
             this.Label1.Name = "Label1";
-            this.Label1.Size = new System.Drawing.Size(127, 23);
+            this.Label1.Size = new System.Drawing.Size(232, 23);
             this.Label1.TabIndex = 6;
-            this.Label1.Text = "Статистика:";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Century", 10F);
-            this.label2.Location = new System.Drawing.Point(12, 32);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(0, 21);
-            this.label2.TabIndex = 7;
+            this.Label1.Text = "Статистика по книгам:";
             // 
             // Print
             // 
-            this.Print.Location = new System.Drawing.Point(16, 187);
+            this.Print.Location = new System.Drawing.Point(16, 253);
             this.Print.Name = "Print";
             this.Print.Size = new System.Drawing.Size(123, 31);
             this.Print.TabIndex = 8;
@@ -64,13 +63,111 @@ namespace library
             this.Print.UseVisualStyleBackColor = true;
             this.Print.Click += new System.EventHandler(this.Print_Click);
             // 
+            // totalBooksLabel
+            // 
+            this.totalBooksLabel.AutoSize = true;
+            this.totalBooksLabel.Font = new System.Drawing.Font("Century", 10F);
+            this.totalBooksLabel.Location = new System.Drawing.Point(12, 32);
+            this.totalBooksLabel.Name = "totalBooksLabel";
+            this.totalBooksLabel.Size = new System.Drawing.Size(231, 21);
+            this.totalBooksLabel.TabIndex = 9;
+            this.totalBooksLabel.Text = "Всего книг в библиотеке - ";
+            // 
+            // unavailableBooksCountLabel
+            // 
+            this.unavailableBooksCountLabel.AutoSize = true;
+            this.unavailableBooksCountLabel.Font = new System.Drawing.Font("Century", 10F);
+            this.unavailableBooksCountLabel.Location = new System.Drawing.Point(13, 53);
+            this.unavailableBooksCountLabel.Name = "unavailableBooksCountLabel";
+            this.unavailableBooksCountLabel.Size = new System.Drawing.Size(204, 21);
+            this.unavailableBooksCountLabel.TabIndex = 10;
+            this.unavailableBooksCountLabel.Text = "Кол-во книг на руках - ";
+            // 
+            // availableBooksCountLabel
+            // 
+            this.availableBooksCountLabel.AutoSize = true;
+            this.availableBooksCountLabel.Font = new System.Drawing.Font("Century", 10F);
+            this.availableBooksCountLabel.Location = new System.Drawing.Point(12, 74);
+            this.availableBooksCountLabel.Name = "availableBooksCountLabel";
+            this.availableBooksCountLabel.Size = new System.Drawing.Size(229, 21);
+            this.availableBooksCountLabel.TabIndex = 11;
+            this.availableBooksCountLabel.Text = "Кол-во книг не на руках - ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 127);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(305, 23);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Статистика по пользователям:";
+            // 
+            // totalUsersCountLabel
+            // 
+            this.totalUsersCountLabel.AutoSize = true;
+            this.totalUsersCountLabel.Font = new System.Drawing.Font("Century", 10F);
+            this.totalUsersCountLabel.Location = new System.Drawing.Point(11, 149);
+            this.totalUsersCountLabel.Name = "totalUsersCountLabel";
+            this.totalUsersCountLabel.Size = new System.Drawing.Size(197, 21);
+            this.totalUsersCountLabel.TabIndex = 13;
+            this.totalUsersCountLabel.Text = "Всего пользователей - ";
+            // 
+            // readersCountLabel
+            // 
+            this.readersCountLabel.AutoSize = true;
+            this.readersCountLabel.Font = new System.Drawing.Font("Century", 10F);
+            this.readersCountLabel.Location = new System.Drawing.Point(11, 170);
+            this.readersCountLabel.Name = "readersCountLabel";
+            this.readersCountLabel.Size = new System.Drawing.Size(114, 21);
+            this.readersCountLabel.TabIndex = 14;
+            this.readersCountLabel.Text = "Читателей - ";
+            // 
+            // employeesCountLabel
+            // 
+            this.employeesCountLabel.AutoSize = true;
+            this.employeesCountLabel.Font = new System.Drawing.Font("Century", 10F);
+            this.employeesCountLabel.Location = new System.Drawing.Point(11, 191);
+            this.employeesCountLabel.Name = "employeesCountLabel";
+            this.employeesCountLabel.Size = new System.Drawing.Size(134, 21);
+            this.employeesCountLabel.TabIndex = 15;
+            this.employeesCountLabel.Text = "Сотрудников - ";
+            // 
+            // mostActiveReaderLabel
+            // 
+            this.mostActiveReaderLabel.AutoSize = true;
+            this.mostActiveReaderLabel.Font = new System.Drawing.Font("Century", 10F);
+            this.mostActiveReaderLabel.Location = new System.Drawing.Point(11, 212);
+            this.mostActiveReaderLabel.Name = "mostActiveReaderLabel";
+            this.mostActiveReaderLabel.Size = new System.Drawing.Size(249, 21);
+            this.mostActiveReaderLabel.TabIndex = 16;
+            this.mostActiveReaderLabel.Text = "Самый активный читатель - ";
+            // 
+            // mostPopularBookLabel
+            // 
+            this.mostPopularBookLabel.AutoSize = true;
+            this.mostPopularBookLabel.Font = new System.Drawing.Font("Century", 10F);
+            this.mostPopularBookLabel.Location = new System.Drawing.Point(13, 95);
+            this.mostPopularBookLabel.Name = "mostPopularBookLabel";
+            this.mostPopularBookLabel.Size = new System.Drawing.Size(235, 21);
+            this.mostPopularBookLabel.TabIndex = 17;
+            this.mostPopularBookLabel.Text = "Самая популярная книга - ";
+            // 
             // statisticForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(311, 245);
+            this.ClientSize = new System.Drawing.Size(473, 296);
+            this.Controls.Add(this.mostPopularBookLabel);
+            this.Controls.Add(this.mostActiveReaderLabel);
+            this.Controls.Add(this.employeesCountLabel);
+            this.Controls.Add(this.readersCountLabel);
+            this.Controls.Add(this.totalUsersCountLabel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.availableBooksCountLabel);
+            this.Controls.Add(this.unavailableBooksCountLabel);
+            this.Controls.Add(this.totalBooksLabel);
             this.Controls.Add(this.Print);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.Label1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "statisticForm";
@@ -84,7 +181,15 @@ namespace library
         #endregion
 
         private System.Windows.Forms.Label Label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button Print;
+        private System.Windows.Forms.Label totalBooksLabel;
+        private System.Windows.Forms.Label unavailableBooksCountLabel;
+        private System.Windows.Forms.Label availableBooksCountLabel;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label totalUsersCountLabel;
+        private System.Windows.Forms.Label readersCountLabel;
+        private System.Windows.Forms.Label employeesCountLabel;
+        private System.Windows.Forms.Label mostActiveReaderLabel;
+        private System.Windows.Forms.Label mostPopularBookLabel;
     }
 }
--- library/statisticForm.cs
+++ library/statisticForm.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Drawing;
+using System.Collections.Generic;
+using System.Data;
 using System.Windows.Forms;
 using System.Drawing.Printing;
 using library.classes;
@@ -11,10 +13,14 @@ namespace library
         public int availableBooksCount = 0;
         public int unAvailableBooksCount = 0;
         public int totalBooksCount = 0;
-
+        public List<DataRow> mostPopularBook = new List<DataRow>();
+        
+        public int usersCount = 0;
         public int readersCount = 0;
-
         public int employeesCount = 0;
+        public List<DataRow> mostActiveReader = new List<DataRow>();
+
+        public string printingText = "";
 
         CMySql DB = new CMySql();
 
@@ -27,18 +33,45 @@ namespace library
         {
             setStatistic();
             printStatistic();
+            setPrintingText();
         }
 
         private void setStatistic()
         {
             totalBooksCount = DB.getCount("SELECT count(id) FROM `books`");
-            employeesCount = DB.getCount("SELECT count(id) FROM `employees`;");
-            readersCount = DB.getCount("SELECT count(id) FROM `readers`;");
+            unAvailableBooksCount = DB.getCount("SELECT count(`books`.`id`) FROM `books` JOIN `readings` ON `readings`.`books_id` = `books`.`id` AND CURDATE() <= `readings`.`dateEnd`;");
+            mostPopularBook = DB.execSelect("SELECT bookDate, name, books_id, count(1) cnt FROM readings JOIN `books` ON `readings`.`books_id` = `books`.`id` WHERE readers_id=(SELECT readers_id FROM readings GROUP BY readers_id ORDER BY count(1) DESC LIMIT 1) GROUP BY books_id ORDER BY cnt DESC LIMIT 1;");
+
+            usersCount = DB.getCount("SELECT count(id) FROM `users`");
+            readersCount = DB.getCount("SELECT count(`users`.`id`) FROM `users` WHERE `accessLevels_id` = 2");
+            mostActiveReader = DB.execSelect("SELECT name, surname, readers_id, count(1) cnt FROM readings JOIN `users` ON `readings`.`readers_id` = `users`.`id` GROUP BY readers_id ORDER BY cnt DESC LIMIT 1;");
         }
 
         private void printStatistic()
         {
-            label2.Text = $"Всего книг в библиотеке: {totalBooksCount} шт.\nВсего читателей: {readersCount} чел.\nВсего персонала: {employeesCount} чел.";
+            totalBooksLabel.Text += totalBooksCount.ToString() + " шт.";
+            unavailableBooksCountLabel.Text += unAvailableBooksCount.ToString() + " шт.";
+            availableBooksCountLabel.Text += (totalBooksCount - unAvailableBooksCount).ToString() + " шт.";
+            mostPopularBookLabel.Text += "[" + mostPopularBook[0]["books_id"].ToString() + "] " + mostPopularBook[0]["name"].ToString() + " " + mostPopularBook[0]["bookDate"].ToString() + "г.";
+
+            totalUsersCountLabel.Text += usersCount.ToString() + " чел.";
+            readersCountLabel.Text += readersCount.ToString() + " чел.";
+            employeesCountLabel.Text += (usersCount - readersCount).ToString() + " чел.";
+            mostActiveReaderLabel.Text += "[" + mostActiveReader[0]["readers_id"].ToString() + "] " + mostActiveReader[0]["name"].ToString() + " " + mostActiveReader[0]["surname"].ToString();
+        }
+
+        private void setPrintingText()
+        {
+            printingText = "Статистика по книгам:"
+                + "\n" + totalBooksLabel.Text 
+                + "\n" + unavailableBooksCountLabel.Text 
+                + "\n" + availableBooksCountLabel.Text 
+                + "\n" + mostPopularBookLabel.Text
+                + "\n" + "Статистика по пользователям:"
+                + "\n" + totalUsersCountLabel.Text
+                + "\n" + readersCountLabel.Text
+                + "\n" + employeesCountLabel.Text
+                + "\n" + mostActiveReaderLabel.Text;
         }
 
         private void Print_Click(object sender, EventArgs e)
@@ -53,7 +86,7 @@ namespace library
 
         void PrintPageHandler(object sender, PrintPageEventArgs e)
         {
-            e.Graphics.DrawString(label2.Text, new Font("Arial", 14), Brushes.Black, 0, 0);
+            e.Graphics.DrawString(printingText, new Font("Arial", 14), Brushes.Black, 0, 0);
         }
     }
 }
